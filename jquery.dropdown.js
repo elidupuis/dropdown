3133d7cc9d9ffe8851ae396bd1067576d57c750b
Eli Dupuis <elidupuis@gmail.com>
2010-09-01 16:14:49 -0600

    v0.3 - minor tweaks

--- jquery.dropdown.js
+++ jquery.dropdown.js
@@ -1,48 +1,43 @@
-/**
- *	jQuery Dropdown plugin
- *	@version 0.2
- *	@date Nov 6, 2009
- *	@author Eli Dupuis
- *	@copyright (c) 2009 Lift Interactive (http://liftinteractive.com)
- *	Dual licensed under the MIT and GPL licenses:
- *	http://www.opensource.org/licenses/mit-license.php
- *	http://www.gnu.org/licenses/gpl.html
- *	Requires: jQuery v1.3.2 or later (most likely works fine with earlier versions, but unteseted)
+/*!
+ * jQuery Dropdown Plugin
+ * Copyright (c) 2010 Eli Dupuis
+ * Version: 0.3 (Sept 1, 2010)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) and GPL (http://creativecommons.org/licenses/GPL/2.0/) licenses.
+ * Requires: jQuery v1.4.2 or later
+ * Based heavily on snippet from (Steve Taylor) http://sltaylor.co.uk/blog/jquery-hover-drop-down-menu-settimeout/
+ */
 
- *	Based heavily on snippet from (Steve Taylor) http://sltaylor.co.uk/blog/jquery-hover-drop-down-menu-settimeout/
-
-*/
 (function($) {
 
-var ver = '0.2';
+var ver = '0.3';
 
 $.fn.dropdown = function(options) {
-	// ;;;debug(this);
 
 	// build main options before element iteration
-	var opts = $.extend({}, $.fn.dropdown.defaults, options);
-	var navTimers = []; 
+	var opts = $.extend({}, $.fn.dropdown.defaults, options),
+	    navTimers = []; 
 
 	return this.each(function() {
 		$this = $(this);
 		// build element specific options
 		var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
 
-		//	start firefox glitch fix:
-		//	remove title attributes because they interfere with the hover() in Firefox:
-		//	http://dev.jquery.com/ticket/5290
-		if ($.browser.mozilla) {
-			$this.removeAttr('title');
-			$this.find('*[title]').each(function(){
-				$(this).removeAttr('title');
-			});
-		}
-		//	end firefox glitch fix.
+        if (opts.mozRemoveTitleAttr) {
+    		//	start firefox glitch fix:
+    		//	remove title attributes because they interfere with the hover() in Firefox:
+    		//	http://dev.jquery.com/ticket/5290
+    		if ($.browser.mozilla) {
+    			$this.removeAttr('title');
+    			$this.find('*[title]').each(function(){
+    				$(this).removeAttr('title');
+    			});
+    		}
+    		//	end firefox glitch fix.
+        };
 
-		$(this).hover(  
+		$this.hover(
 			function (e) {
-				var id = $.data(this);
-				var $this = $(this);
+				var id = $.data(this), $this = $(this);
 				navTimers[id] = setTimeout( function() {
 					$this.find(o.child).fadeIn(o.speedIn);
 					navTimers[id] = "";
@@ -60,21 +55,14 @@ $.fn.dropdown = function(options) {
 };
 
 //
-// private function for debugging
-//
-// ;;;function debug($obj) {
-// ;;;	if (window.console && window.console.info)
-// ;;;	window.console.info('dropdown element count: ' + $obj.size());
-// ;;;};
-
-//
 // plugin defaults
 //
 $.fn.dropdown.defaults = {
 	delay: 100,
-	speedIn: 300,
-	speedOut: 200,
-	child: 'ul'
+	speedIn: 200,
+	speedOut: 150,
+	child: 'ul',
+	mozRemoveTitleAttr: true
 };
 
 //	public function/method
